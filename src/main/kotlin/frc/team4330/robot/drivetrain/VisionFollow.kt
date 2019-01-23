02d3cb7725c5e7888127c7571d5cdf1ef5d22402
Sithija Dasun Gunasinghe <sparks10.sg@gmail.com>
2019-01-23 17:04:09 -0600

    changed vision to work in kotlin from ri3d code

--- src/main/kotlin/frc/team4330/robot/drivetrain/VisionFollow.kt
+++ src/main/kotlin/frc/team4330/robot/drivetrain/VisionFollow.kt
@@ -1,7 +1,5 @@
 package frc.team4330.robot.drivetrain
 
-import frc.team4330.robot.drivetrain.Vision
-
 //  U   N   W   O   R   K   I   N   G
 class VisionFollow {
     lateinit var vision: Vision
@@ -18,8 +16,6 @@ class VisionFollow {
     }
 
     fun initialize() {
-        vision = Vision("Vision", 0.11, 0.0, .01)
-
         vision.init()
     }
 
